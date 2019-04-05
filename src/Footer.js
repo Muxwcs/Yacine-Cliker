2cac239c55f1ba05a0d2dbdc04fb51f1afaeeeca
Gagnant Nicolas <gagnantnicolas64@gmail.com>
2019-04-05 11:32:33 +0200

    Structur complet pour le Footer

--- src/Footer.js
+++ src/Footer.js
@@ -11,24 +11,27 @@ export default class Footer extends React.Component {
       <Row className="row no-gutter">
       
         
-          
-          <Col sm="1" lg="1"><NavItem>
-            <NavLink disabled href="#">©</NavLink>
-          </NavItem></Col>
           <Col sm="8" lg="6"><NavItem>
             <NavLink disabled href="#">TEAM WILDER CLICKER : Stéphane Van Pé, Nathalie Belotte, Stéphane Jacot, Nicolas Gagnant et Guillaume Ribéroux </NavLink>
           </NavItem></Col>
-          <Col sm="1" lg={{size: 1, offset: 2}}><NavItem>
+
+          <Row className="rows">
+
+          <Col class="col-sm-1 col-lg-1"><NavItem>
+            <NavLink disabled href="#">©</NavLink>
+          </NavItem></Col>
+        
+          <Col class="col-sm-push-1 col-lg-1"><NavItem>
             <NavLink href="https://www.facebook.com"><img src="https://img.icons8.com/color/28/000000/facebook.png"/>  </NavLink>
           </NavItem></Col>
-          <Col sm="1" lg="1"><NavItem>
+          <Col class="col-sm-push-1 col-lg-1"><NavItem>
             <NavLink href="https://www.twitter.com" ><img src="https://img.icons8.com/color/28/000000/twitter.png"/>  </NavLink>
           </NavItem></Col>
-          <Col sm="1" lg="1"><NavItem>
+          <Col class="col-sm-push-1 col-lg-1"><NavItem>
             <NavLink href="https://www.instagram.com" ><img src="https://img.icons8.com/color/28/000000/instagram-new.png"/>  </NavLink>
           </NavItem></Col>
        
-      
+      </Row>
       </Row>
     </div>
     );

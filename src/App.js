7cd781059a2a23737ad4dd4560e26b6a5b24288b
Guillaume Ribéroux <guillaume.riberoux@gmail.com>
2019-04-09 16:00:45 +0200

    json update, index.html update, App.css deleted, App.js update, init_app.css created, add bg image in img directory

--- src/App.js
+++ src/App.js
@@ -1,40 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Container,
-  Row,
-  Col,
-  Jumbotron,
-  Button
-} from 'reactstrap';
+import React, { Component } from "react";
 
+//  STYLE
+import "./init_app.css";
+
+// COMPONENTS REACSTRAP
+import { Row, Col } from "reactstrap";
+
+
+// HOMEMADE COMPONENTS
+// import NameForm from './NameForm';
+// import Navi from './Navbar.js';
+import Counter from "./Counter";
+// import Cookie from './Cookie.js';
+// import LeaderBoard from './LeaderBoard.js';
+// import Add_ons from './Add_ons.js';
+// import Footer from './Footer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* <NameForm/> */}
+        <header className="App-header">{/* <Navbar /> */}</header>
+        <div className="BackGround">
+          <Row id="skeleton_one">
+            <Col sm="12" lg={{ size: 6, offset: 3 }}>
+              {/* <img
+                href
+                src="https://fakeimg.pl/640x360/"
+                className="img_fictive"
+                alt="fictive"
+              /> */}
+              <Counter />
+            </Col>
+          </Row>
+          <Row id="skeleton_two">
+            <Col sm="12" lg="3">
+              <img
+                href
+                src="https://fakeimg.pl/640x360/"
+                className="img_fictive"
+                alt="fictive"
+              />
+              {/* Add-ons */}
+            </Col>
+            <Col sm="12" lg="6">
+              <img
+                href
+                src="https://fakeimg.pl/640x360/"
+                className="img_fictive"
+                alt="fictive"
+              />
+              {/* Cookie */}
+            </Col>
+            <Col sm="12" lg="3">
+              <img
+                href
+                src="https://fakeimg.pl/640x360/"
+                className="img_fictive"
+                alt="fictive"
+              />
+              {/* LeaderBoard */}
+            </Col>
+          </Row>
+        <footer className="App-footer">{/* <Footer />  */}</footer>
+        </div>
       </div>
     );
   }

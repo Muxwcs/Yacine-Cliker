d9a3983b73ce0fdce953abf7e285762032a16877
Guillaume Ribéroux <guillaume.riberoux@gmail.com>
2019-04-29 14:19:13 +0200

    cleaning app.js done

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Navi />{" "}
+          <Navi />
         </header>
         <div className="BackGround">
           <Row id="skeleton_one">
